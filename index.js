1ad781b3858d36486e9c9dbf031eb4ffeaf32b47
Nick Lavery <tuneboynick@gmail.com>
2020-07-30 20:08:18 -0400

    Group work progress

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 //  Hints: What are the basics needed for an express app?
 //         What is missing to configure pug as the view engine
 const scores = require('./scores')
+const express = require('express')
+const app = express()
+
+const router = express.Router()
+
+app.use(router)
+
 
 
 // No Changes are needed below this line!!
